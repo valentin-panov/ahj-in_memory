fd0d8ba44c0a9ae05a3882cc6c3de9d3d93d3e53
Valentin Panov <valentin.panov@me.com>
2021-04-25 01:19:57 +0300

    halfway done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import './css/style.css';
-import  './js/app';
+import './js/app';
 
 // entry point for webpack
--- src/js/engine/sorting.js
+++ src/js/engine/sorting.js
@@ -1,13 +1,13 @@
-export default class Sorting {
-  constructor()
-  gamePlay() {
-    setInterval(() => {
-      let newHole = this.activeHole;
-      while (newHole === this.activeHole) {
-        newHole = Math.floor(Math.random() * this.divArray.length);
-      }
-      this.activeHole = newHole;
-      this.moveGoblin(this.activeHole);
-    }, 1000);
-  }
-}
+// export default class Sorting {
+//   constructor()
+//   gamePlay() {
+//     setInterval(() => {
+//       let newHole = this.activeHole;
+//       while (newHole === this.activeHole) {
+//         newHole = Math.floor(Math.random() * this.divArray.length);
+//       }
+//       this.activeHole = newHole;
+//       this.moveGoblin(this.activeHole);
+//     }, 1000);
+//   }
+// }
